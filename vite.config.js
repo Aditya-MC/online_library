31f259189069176e9bc2c793038c4cd767c19c52
Aditya-MC <adityamc364@gmail.com>
2025-08-15 21:40:47 +0530

    fixed 'base' errors

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'online_library',
+  base:'/online_library/',
   plugins: [react()],
 })
